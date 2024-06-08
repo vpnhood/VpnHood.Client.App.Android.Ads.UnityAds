48a3f3c4c018d016a8b98460e1dce1ea2abc666e
Trudy <trudy@vpnhood.com>
2024-06-08 02:23:59 -0700

    init

---
+++ VpnHood.Client.App.Android.Ads.UnityAds/Additions/AdModule.cs
@@ -0,0 +1,12 @@
+﻿using Com.Unity3d.Services.Core.Webview.Bridge;
+
+namespace VpnHood.Client.App.Droid.Ads.VhUnityAds.Additions
+{
+    public abstract partial class AdModule
+    {
+        public void ExecuteAdOperation(IWebViewBridgeInvoker p0, Java.Lang.Object p1)
+        {
+            
+        }
+    }
+}
---
+++ VpnHood.Client.App.Android.Ads.UnityAds/Class1.cs
@@ -0,0 +1,6 @@
+namespace VpnHood.Client.App.Android.Ads.UnityAds
+{
+    public class Class1
+    {
+    }
+}
